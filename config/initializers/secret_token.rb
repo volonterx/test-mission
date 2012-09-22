206c5df48ca1992e5e1e99257bcfa4680b080755
Yuriy Omelchuk <jurgen@upscript.com>
2012-09-22 15:40:57 +0300

    fresh rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KodoTest::Application.config.secret_token = 'd952fac9ed16c95d262b0c5f4f822011c7081f008f5cfcc25c0c0473c9960065493cf487d0c1e21048befab2d0ea71efc8356f48cfdbd38ac9edfc954200d553'
