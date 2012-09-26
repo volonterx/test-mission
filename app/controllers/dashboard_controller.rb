5b7a7ca8fc476232b725e8dbb5e8bb9c589a5cd9
volonterx <volonterx@volonterx-laptop.(none)>
2012-09-26 18:57:58 +0300

    [-] удален метод current_user_is_complited_registration и тест к нему

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,12 +1,7 @@
 class DashboardController < ApplicationController
-  before_filter :authenticate_user!, :current_user_is_complited_registration
+  before_filter :authenticate_user!
   
   def index
   end
-
-private
   
-  def current_user_is_complited_registration
-    redirect_to welcome_index_path, notice: flash[:notice] unless current_user.show_welcome_page?
-  end
 end
--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -5,7 +5,6 @@ describe DashboardController do
   before(:each) do
     @user = FactoryGirl.create :user 
     sign_in @user
-    controller.current_user.stub!(:show_welcome_page?).and_return(true)
   end
 
   describe "GET #index" do
@@ -13,17 +12,6 @@ describe DashboardController do
       get 'index'
       response.should render_template :index
     end
-  end
-
-  describe "#current_user_is_complited_registration" do
-    it "redirects to the /welcome page with notice if user does not complite registration" do
-      notice = flash[:notice]
-      controller.current_user.stub!(:show_welcome_page?).and_return(false)
-      get :index
-      flash[:notice].should eq(notice)
-      response.should redirect_to(welcome_index_path)
-    end
-
   end
 
 end
